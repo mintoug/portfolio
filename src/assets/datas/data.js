9de3a21d9e8088315b5e16da9957d91941a822c9
Mandhouj A <nouitou@yahoo.fr>
2022-06-26 15:57:42 +0200

    rectifications parcours

--- src/assets/datas/data.js
+++ src/assets/datas/data.js
@@ -1,36 +1,37 @@
 export const items = [
 {
     title: "2022",    
-     cardTitle: "Développeur Front-end",
-     cardSubtitle:"obtention du titre RNCP en développement web",
-    cardDetailedText: "Recherche d'emploi"
+     cardTitle: "Développeuse Front-end",
+     cardSubtitle:"titre RNCP",
+    cardDetailedText: "recherche d'emploi"
 }    
 ,
 {
     title: "2020-2022",    
-    cardTitle: "Développeur Front-end en formation",
-    cardSubtitle:" Ecole à distance : Openclasseroom",
-    cardDetailedText: "réussite de mon parcours par la réalisation de projets"
+    cardTitle: "Développeuse en formation",
+    cardSubtitle:" Ecole : Openclasseroom",
+    cardDetailedText: "réussite de mon parcours par la réalisation de projets divers"
 },
 {
     title: "2019-2020",  
     cardTitle: "Projet de conversion professionnelle",
     cardSubtitle:"vers les métier du web",
-    cardDetailedText: "formation en autodidacte"
+    cardDetailedText: "formation en autodidacte : udemy, youtube..."
 },
 {
     title: "2012-2018",
     cardSubtitle:"Manman en plein temps",
+    cardDetailedText: "choix personnel de m'occuper des enfants en bas âge."
 },
 
 {   title:"2006-2012",
     cardTitle: "Stages",
-    cardSubtitle:"lieu hopiataux de monastir et mehdia (en Tunisie)",
+    cardSubtitle:"lieu hopiataux de monastir et mahdia (en Tunisie)",
     cardDetailedText: "assurer des consultations, assister à des séminaires, faire des présentations orales"
 },
 {    title: "2001-2005",
-    cardTitle: "Etudes  Universitaires",
-    cardSubtitle:"etudes en tunisie , faculté de médecine de Monastir",
+    cardTitle: "université",
+    cardtitle:"etudes en tunisie , faculté de médecine de Monastir",
     cardDetailedText: "domaine de la santé, études médicales"
 },
 {  title: "2000",
