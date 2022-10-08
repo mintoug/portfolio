3397ec0b7f1aca4c23d1e91208b90b0c272540e9
Mandhouj A <nouitou@yahoo.fr>
2022-10-08 18:40:04 +0200

    mise à jour du datas

--- src/assets/datas/data.js
+++ src/assets/datas/data.js
@@ -1,38 +1,32 @@
 export const items = [
 {
     title: "2022",    
-     cardTitle: "Développeuse d'applications Front-end",
-     cardSubtitle:"Titre RNCP : Bac +3/4ans",
+    cardTitle: "Développeuse d'applications Front-end",
     cardDetailedText:"React-Javascript"
 }    
 ,
 {
     title: "2020-2022",    
-    cardTitle: "Formation",
-    cardSubtitle:" Openclasserooms",
+    cardTitle: "Formation Openclasserooms",
     cardDetailedText: "Réussite par la réalisation de projets puis passage au Jury"
 },
 {
     title: "2020",  
     cardTitle: "Conversion professionnelle",
-    cardSubtitle:"vers les métiers du web",
     cardDetailedText: "formation en autodidacte"
 },
 {
-    title: "2012-2018",
-    cardSubtitle:"Manman en plein temps",
-    cardDetailedText: "des enfants en bas âge."
+    title: "2013-2018",
+    cardDetailedText: "Occupation de mes enfants en bas âge."
 },
 
 {   title:"2006-2012",
-    cardTitle: "Stages",
-    cardSubtitle:"lieu hôpitaux (en Tunisie)",
+    cardTitle: "Stages en médecine",
     cardDetailedText: "assurer des consultations, assister à des séminaires"
 },
-{    title: "2001-2005",
-    cardTitle: "Université",
-    cardSubtitle:"faculté de médecine de Monastir (Tunisie)",
-    cardDetailedText: "domaine de la santé, médecine"
+{   title: "2001-2005",
+    cardTitle: "Etudes en médeçine",
+    cardDetailedText: "faculté de medeçine Monastir (Tunisie)"
 },
 {  title: "2000",
    cardTitle: "Bac maths",
